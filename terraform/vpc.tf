5e75aecda86520d8e31a896afe1dcf631591e2b1
Misha Manulis <misha@manulis.com>
2016-04-28 23:22:01 -0500

    Setup VPC
    - Just the VPC with basics
        - Internet gateway to be used by the VPC to get outside
        - Default routing table to access outside

---
+++ terraform/vpc.tf
@@ -0,0 +1,26 @@
+# Specify the provider and access details
+provider "aws" {
+    region = "${var.aws_region}"
+}
+
+# Create a VPC to launch our instances into
+resource "aws_vpc" "default" {
+    cidr_block = "10.0.0.0/16"
+    tags {
+        Name = "kuber-playground"
+    }
+}
+
+resource "aws_internet_gateway" "default" {
+    vpc_id = "${aws_vpc.default.id}"
+    tags {
+        Name = "kuber-playground"
+    }
+}
+
+resource "aws_route" "internet_access" {
+    route_table_id          = "${aws_vpc.default.main_route_table_id}"
+    destination_cidr_block  = "0.0.0.0/0"
+    gateway_id              = "${aws_internet_gateway.default.id}"
+}
+
